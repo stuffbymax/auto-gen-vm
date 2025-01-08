69864405e85eb7e78da65cdb533a41c44dffc5ac
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2025-01-08 23:17:45 +0000

    Update auto-vm.sh

--- auto-vm.sh
+++ auto-vm.sh
@@ -1,51 +1,101 @@
 #!/bin/bash
 
-# Check if VBoxManage is installed
-if ! command -v VBoxManage &> /dev/null
-then
-    echo "VBoxManage could not be found. Please install VirtualBox."
-    exit 1
+# Ensure the script is run as a regular user (not root)
+if [[ $EUID -eq 0 ]]; then
+   echo "This script must NOT be run as root. Please run it as a regular user." 
+   exit 1
 fi
 
-# Variables (customize as needed or pass as arguments)
-VM_NAME=${1:-demovm} # Default to 'demovm' if no name is provided
-RAM_SIZE=${2:-1024}  # Default RAM size 1024MB
-VRAM_SIZE=${3:-16}   # Default VRAM size 16MB
-DISK_SIZE=${4:-10000} # Default disk size 32GB
-ISO_PATH=${5:-/home/zdislav/Downloads/openmandriva.rome-23.01-plasma.x86_64.iso} # Path to ISO image
-NIC_TYPE="82540EM"# Network card type for NAT
-BRIDGE_ADAPTER="#" # Bridge adapter name
+# Function to display a menu and get the user's choice
+function show_menu() {
+    echo "-----------------------------------------"
+    echo "        VirtualBox Management Script      "
+    echo "-----------------------------------------"
+    echo "1. List all VMs"
+    echo "2. Create a new VM"
+    echo "3. Start a VM"
+    echo "4. Stop a VM"
+    echo "5. Delete a VM"
+    echo "6. Exit"
+    echo "-----------------------------------------"
+    read -p "Choose an option [1-6]: " choice
+    echo ""
+    return $choice
+}
 
-# Create a new VM
-VBoxManage createvm --name "$VM_NAME" --register
+# Function to list all VMs
+function list_vms() {
+    echo "Listing all VMs..."
+    VBoxManage list vms
+}
 
-# Set OS type to Linux 64-bit
-VBoxManage modifyvm "$VM_NAME" --ostype Linux_64
+# Function to create a new VM
+function create_vm() {
+    read -p "Enter the name of the new VM: " vm_name
+    read -p "Enter the type of OS (e.g., Linux, Windows): " os_type
+    read -p "Enter the version of the OS (e.g., Ubuntu_64, Windows10_64): " os_version
+    read -p "Enter the amount of RAM in MB (e.g., 2048): " ram_size
+    read -p "Enter the size of the virtual disk in MB (e.g., 20000): " disk_size
 
-# Configure RAM and VRAM
-VBoxManage modifyvm "$VM_NAME" --memory "$RAM_SIZE" --vram "$VRAM_SIZE"
+    echo "Creating VM..."
+    VBoxManage createvm --name "$vm_name" --ostype "$os_version" --register
+    VBoxManage modifyvm "$vm_name" --memory "$ram_size" --vram 16 --acpi on --boot1 dvd --nic1 nat
+    VBoxManage createhd --filename "$HOME/VirtualBox VMs/$vm_name/$vm_name.vdi" --size "$disk_size" --variant Standard
+    VBoxManage storagectl "$vm_name" --name "SATA Controller" --add sata --controller IntelAhci
+    VBoxManage storageattach "$vm_name" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HOME/VirtualBox VMs/$vm_name/$vm_name.vdi"
 
-# Create virtual hard disk
-VBoxManage createhd --filename "$VM_NAME.vdi" --size "$DISK_SIZE"
+    echo "VM $vm_name created successfully!"
+}
 
-# Add storage controller and attach hard disk
-VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controller IntelAHCI
-VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VM_NAME.vdi"
+# Function to start a VM
+function start_vm() {
+    read -p "Enter the name or UUID of the VM to start: " vm_name
+    VBoxManage startvm "$vm_name" --type headless
+    echo "VM $vm_name is starting in headless mode."
+}
 
-# Attach ISO image to boot from
-VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium "$ISO_PATH"
+# Function to stop a VM
+function stop_vm() {
+    read -p "Enter the name or UUID of the VM to stop: " vm_name
+    VBoxManage controlvm "$vm_name" poweroff
+    echo "VM $vm_name is shutting down."
+}
 
-# Set boot order
-VBoxManage modifyvm "$VM_NAME" --boot1 dvd --boot2 disk --boot3 none --boot4 none
+# Function to delete a VM
+function delete_vm() {
+    read -p "Enter the name or UUID of the VM to delete: " vm_name
+    VBoxManage unregistervm "$vm_name" --delete
+    echo "VM $vm_name has been deleted."
+}
 
-# Configure network settings
-VBoxManage modifyvm "$VM_NAME" --nic1 nat --nictype1 "$NIC_TYPE"
-VBoxManage modifyvm "$VM_NAME" --nic1 bridged --nictype1 "$NIC_TYPE" --bridgeadapter1 "$BRIDGE_ADAPTER"
+# Main script logic
+while true; do
+    show_menu
+    choice=$?
 
-# Enable Physical Address Extension (PAE)
-VBoxManage modifyvm "$VM_NAME" --pae on
-
-# Start the VM
-VBoxManage startvm "$VM_NAME"
-
-echo "Virtual machine '$VM_NAME' has been created and started successfully!"
+    case $choice in
+        1)
+            list_vms
+            ;;
+        2)
+            create_vm
+            ;;
+        3)
+            start_vm
+            ;;
+        4)
+            stop_vm
+            ;;
+        5)
+            delete_vm
+            ;;
+        6)
+            echo "Exiting VirtualBox management script. Goodbye!"
+            break
+            ;;
+        *)
+            echo "Invalid choice. Please try again."
+            ;;
+    esac
+    echo ""
+done
